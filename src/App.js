3febd71ff7c7f3287f1c4b343054fb44d562df4a
t.manikanta reddy <thikkavarapureddy@ninjacart.com>
2020-04-13 16:04:30 +0530

    Redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
   constructor(props) {
@@ -13,11 +17,13 @@ class App extends Component {
   }
   render() {
     return (
-      <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
-    </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
