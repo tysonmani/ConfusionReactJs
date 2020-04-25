dad2e47a5a74138f1c24f9bb4e05629fd6485daf
t.manikanta reddy <thikkavarapureddy@ninjacart.com>
2020-04-25 12:59:04 +0530

    UnecessaryStuffRemoved

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { FadeTransform, Fade, Stagger } from "react-animation-components";
+import {Fade} from "react-animation-components";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
 
